c7bf80e612d15207f6d2161ce7f581cd635db038
kcdev <52672187+kcdevillier@users.noreply.github.com>
2020-03-22 11:21:26 -0600

    final

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,53 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+//get a reference to the table body and search button
+var tbody = d3.select("tbody");
+var search = d3.select("#filter-btn");
+
+
+
+//create html table with all data values
+function updateTable(filteredData){
+
+    filteredData.forEach((alienSight)=> { 
+        var row = tbody.append("tr");
+        Object.entries(alienSight).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });  
+    });
+};
+
+updateTable(tableData);
+
+//on submit function, reset html table with values that match search results
+search.on("click", function() { 
+    
+    //clear html table
+    tbody.html(""); 
+
+    //get reference for input value 
+    dateValue = d3.select("#datetime").property("value");
+    cityValue = d3.select("#city").property("value");
+    stateValue = d3.select("#state").property("value");
+    var filteredData = tableData;
+
+    if (dateValue!=""){
+        filteredData = filteredData.filter(filteredData => filteredData.datetime === dateValue);
+        // updateTable(dateData);
+    }
+    if (cityValue!=""){
+        filteredData = filteredData.filter(filteredData => filteredData.city === cityValue.toLowerCase());
+        // updateTable(cityData);
+    }
+    if (stateValue!=""){
+        filteredData = filteredData.filter(filteredData => filteredData.state === stateValue.toLowerCase());
+        // updateTable(stateData);
+    }
+    updateTable(filteredData);
+  });
+
+
+
+
